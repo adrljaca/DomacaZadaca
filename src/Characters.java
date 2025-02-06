d00fee6ff708349886f22da2058ec284e541f9e8
adrljaca <arijand16@gmail.com>
2025-02-06 14:19:28 +0100

    DZ Characters - updated

--- src/Characters.java
+++ src/Characters.java
@@ -1,7 +1,14 @@
+//DZ 05-02-2025
+
 import java.util.Scanner;
 
 public class Characters {
+
+    public static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
+
+        //1. Zadatak
         //Napišite program koji učitava niz znakova sa standardnog ulaza i ispisuje koliko ima slova,
         //koliko brojeva i koliko "ostalih znakova".
         //Koristiti clasu character!!!
@@ -9,9 +16,6 @@ public class Characters {
         //Slova: 7
         //Brojevi: 8
         //Ostali znakovi: 5
-
-        //1. Zadatak
-        Scanner sc = new Scanner(System.in);
 
         System.out.println("Upišite rečenicu: ");
         String unos = sc.nextLine();
@@ -21,13 +25,13 @@ public class Characters {
         int znakovi = 0;
 
         for (int i = 0; i < unos.length(); i++) {
-            char trenutniChar = unos.charAt(i);
+            char c = unos.charAt(i);
 
-            if (Character.isLetter(trenutniChar)) {
+            if (Character.isLetter(c)) {
                 slova++;
-            } else if (Character.isDigit(trenutniChar)) {
+            } else if (Character.isDigit(c)) {
                 brojevi++;
-            } else {
+            } else if (c == '.' || c == ',' || c == '?' || c == '"' || c == '!' || c == '-') {
                 znakovi++;
             }
         }
@@ -36,5 +40,21 @@ public class Characters {
         System.out.println("\tSlova: " + slova);
         System.out.println("\tBrojeva: " + brojevi);
         System.out.println("\tZnakova: " + znakovi);
+
+        //2. Zadatak
+        //Napišite program u Javi koji će učitati niz znakova (string) sa standardnog
+        //ulaza i okrenuti redoslijed riječi u unesenom stringu. Prikazati transformirani
+        //string s obrnutim redoslijedom riječi.
+        //Primjer ulaza: Dobar dan svima
+        //Primjer izlaza: svima dan Dobar
+
+        System.out.println("Unesite nekakav string znakova: ");
+        String input = sc.nextLine();
+
+        String[] rijeci = input.split(" ");
+
+        for (int i = rijeci.length - 1; i >= 0; i--) {
+            System.out.print(rijeci[i] + " ");
+        }
     }
 }
