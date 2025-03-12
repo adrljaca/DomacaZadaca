69e243f3bd39f58130434fec26d02c3a77a6ebf4
adrljaca <arijand16@gmail.com>
2025-03-12 20:15:09 +0100

    updated PripremaZaModul

--- src/PripremaZaModulIspit/JavaPrint.java
+++ src/PripremaZaModulIspit/JavaPrint.java
@@ -4,9 +4,15 @@ package PripremaZaModulIspit;
 
 public class JavaPrint {
     public static void main(String[] args) {
-        System.out.println("     J      a      v     v     a ");
-        System.out.println("     J    a   a     v   v    a   a");
-        System.out.println(" J   J   aaaaaaa     V V    aaaaaaa");
-        System.out.println("  J J   a       a     V    a       a");
+        String[] polje = {
+                "     J      a      v     v     a ",
+                "     J    a   a     v   v    a   a",
+                " J   J   aaaaaaa     V V    aaaaaaa",
+                "  J J   a       a     V    a       a"
+        };
+
+        for (String p : polje) {
+            System.out.println(p);
+        }
     }
 }
